8826ca091a507b003798909d50fca88cefd3e8a1
Jing <liujingyun0209@gmail.com>
2017-10-15 16:40:25 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,9 +1,10 @@
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, KFold
 from sklearn.utils import shuffle
+from sklearn.metrics import make_scorer, accuracy_score, precision_recall_fscore_support
 import numpy as np
 
-from preprocess.vectorize import init
 from linear.NB import NaiveBayes
+
 # .... import a bunch of models here...
 
 
@@ -14,18 +15,39 @@ def PCA(X, n_component):
     eig_pairs.sort(key=lambda x: x[0], reverse=True)
     eig_vec = [p[1] for p in eig_pairs] ## sorted eigen_vec
     V = np.stack(eig_vec,axis=1)
-	w = X.dot(V[:n_component].T)
-	return w
+    w = X.dot(V[:n_component].T)
+    return w
 
 
 def evaluate(models):
-    ## evaluate a list of models
-    X,y = init()
-    X, y = shuffle(X,y)
-    for model in models:
-        scores = cross_val_score(model, X, y, cv = 10)
-        print(np.mean(scores))
+    X = np.load("preprocess/X.npy")
+    y = np.load("preprocess/Y.npy").astype(int)
+    # print(X.shape)
+    # print(y.shape)
+    X, y = shuffle(X,y.reshape((-1,)))
+    acc_scorer = make_scorer(accuracy_score)
 
+    kf = KFold(n_splits=10)
+    for model in models:
+        acc_avg = []
+        f1_0_avg = []
+        f1_1_avg = []
+        for train_index, test_index in kf.split(X):
+            X_train, X_valid = X[train_index], X[test_index]
+            y_train, y_valid = y[train_index], y[test_index]
+            model.fit(X_train, y_train)
+
+            yp_valid = model.predict(X_valid)
+            precision, recall, f1, _ = precision_recall_fscore_support(y_valid, yp_valid)
+            acc = accuracy_score(y_valid, yp_valid)
+          
+            acc_avg.append(acc)
+            f1_0_avg.append(f1[0])
+            f1_1_avg.append(f1[1])
+   
+        print("accuracy mean",np.mean(np.array(acc_avg)))
+
+    
 
 if __name__ == "__main__":
     clf = [NaiveBayes(smoothing = 1)]
