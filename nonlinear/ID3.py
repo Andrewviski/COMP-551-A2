1f5b4e1d268b0d4a76fbac7871b7f85843e9664f
Andre <andree_kaba93@hotmail.com>
2017-10-18 21:25:55 -0400

    ID3 Donegit add nonlinear/ID3.py

--- nonlinear/ID3.py
+++ nonlinear/ID3.py
@@ -2,24 +2,59 @@
 import sys
 import BasicSelector
 import random
+import math
+from pprint import pprint
+import numpy as np
 
-max_depth = 100
+max_depth = 20
 min_size = 3
+Nbuckets=90
+
+def split(X,Y,feature):
+    unique_values=set(X[:,feature])
+    buckets={}
+    for value in unique_values:
+        buckets[int(value)%Nbuckets]={'X':[],'Y':[]}
+
+    for i in range(0,len(X)):
+        buckets[int(X[i][feature])%Nbuckets]['X'].append([x for i,x in enumerate(X[i]) if i!=feature])
+        buckets[int(X[i][feature])%Nbuckets]['Y'].append(Y[i])
+    return (buckets)
+
+def entropy(Y):
+    total_size=len(Y)
+    class_size={}
+    for e in Y:
+        if e  in class_size:
+            class_size[e]+=1
+        else:
+            class_size[e]=1
+    return sum([(-(subset_size/total_size)*math.log2(subset_size/total_size)) for subset_size in class_size.values()])
+
+def information_gain(feature,X,Y):
+    splits=split(X,Y,feature).values()
+    sz=len(X)
+    score=0
+    for subset in splits:
+        score+=(len(subset['X'])/sz)*entropy(subset['Y'])
+    return entropy(Y)-score
+
+def get_best_attribute(X,Y):
+    return max((information_gain(feature,X,Y),feature) for feature in range(0,len(X[0])))
+
 
 class ID3:
     def __init__(self):
-        self.l = None
-        self.r = None
-        self.partitionFeature = -1
-        self.partitionValue = -1
+        self.children = []
+
         self.isLeaf = False
+        self.partitionFeature = -1
+        self.partitionValue=-1
         self.Predication = -1
 
     def pick_majority(self, Y):
-        outcomes = [v for v in Y]
-        return max(set(outcomes), key=outcomes.count)
-
-
+        classes = [v for v in Y]
+        return max(set(classes), key=classes.count)
 
     def fit(self, X, Y, depth):
         nclasses = len(set(_ for _ in Y))
@@ -27,43 +62,49 @@ class ID3:
             self.isLeaf = True
             self.predication = self.pick_majority(Y)
         else:
-            best = BasicSelector.get_best_split(X, Y,random=False,trials=20)
-            (lx, ly, rx, ry) = best['partitions']
-            self.partitionFeature = best['feature']
-            self.partitionValue = best['value']
-
-            if len(lx)!=0:
-                self.l = ID3()
-                self.l.fit(lx, ly, depth + 1)
-
-            if len(rx) != 0:
-                self.r = ID3()
-                self.r.fit(rx, ry, depth + 1)
+            best=get_best_attribute(X,Y)
+            subsets = split(X,Y,best[1])
+            print("splitting on %d"%best[1])
+            for splitValue in subsets:
+                subset=subsets[splitValue]
+                t=ID3()
+                t.fit(np.array(subset['X']),np.array(subset['Y']),depth+1)
+                t.partitionFeature=int(best[1])
+                t.partitionValue=splitValue
+                print("With value %s"%splitValue)
+                self.children.append(t)
 
     def predict(self, X):
-        if self.isLeaf:
-            return self.predication
-        elif X[self.partitionFeature] <= self.partitionValue or self.r==None:
-            return self.l.predict(X)
-        elif X[self.partitionFeature] > self.partitionValue or self.l==None:
-            return self.r.predict(X)
+        if self.isLeaf==True:
+           return self.predication
+        else:
+            for child in self.children:
+                if X[child.partitionFeature]==child.partitionValue:
+                    return child.predict(X)
+            mn=np.inf
+            bestChild=None
+            for child in self.children:
+                #print(X[child.partitionFeature] +"   vs   "+int(child.partitionValue))
+                if abs(int(X[child.partitionFeature])-int(child.partitionValue))<mn:
+                    #print("new min= "+str(mn))
+                    mn=abs(int(X[child.partitionFeature])-int(child.partitionValue))
+                    bestChild=child
+            return bestChild.predict(X)
 
     def __print_tree(self, depth=0):
         if not self.isLeaf:
             print('%s[X%d < %s]' % (depth * ' ', (self.partitionFeature + 1), self.partitionValue))
-            if self.l!=None:
-                self.l.__print_tree(depth + 1)
-            if self.r != None:
-                self.r.__print_tree(depth + 1)
+            for child in self.children:
+                child.__print_tree(depth + 1)
         else:
-            print('%s[%s]' % (depth * ' ', (self.predication)))
+            print('%s[%s]' % (depth * ' ', str(self.predication)))
 
     def print(self):
         self.__print_tree(0)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 1:
         print("[*] Reading Training data...")
         data = []
         for line in open(sys.argv[1]):
@@ -71,28 +112,32 @@ if __name__ == "__main__":
             for value in line.split(","):
                 value = value.strip()
                 if value != "":
-                    tempRow.append(value)
+                    if not value.isdigit():
+                        tempRow.append(0)
+                    else:
+                        tempRow.append(value)
             data.append(tempRow)
 
         print("[*] Shuffling data...")
         random.shuffle(data)
         #pprint(data)
 
-        Y = [row[0] for row in data]
-        X = [row[1:] for row in data]
+        Y = np.array([row[-1] for row in data if len(row)>0])
+        X = np.array([row[:-2] for row in data if len(row)>0])
+        #pprint(split(X,Y,get_best_attribute(X,Y)[1]))
         tree = ID3()
         print("[*] Training tree...")
         tree.fit(X, Y, 0)
-        tree.print()
+        #tree.print()
 
         print("[*] Testing...")
         correct = 0
         total=0
         for line in open(sys.argv[2]):
             row=[_ for _ in line.strip().split(",")]
-            pred = tree.predict(row[1:])
-            #print("ID3 predict %s, actual class is %s" % (pred, row[-1]))
-            if pred == row[0]:
+            pred = tree.predict(np.array(row[:-2]))
+            print("ID3 predict %s, actual class is %s" % (pred, row[-1]))
+            if pred == row[-1]:
                 correct += 1
             total+=1
         print('Accuracy= %f%%' % (correct * 100 / float(total)))
