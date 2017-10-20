6c94442f9f6cc98f04f9fea1b68f3e896f48b44c
Paul Wu <jiapeng.wu@mail.mcgill.ca>
2017-10-19 22:50:51 -0400

    Deleted most files in preprocess folder, updated test_on_packages

--- preprocess/preprocess.bash
+++
@@ -1,4 +0,0 @@
-python remove_url.py > url_removed.txt
-python remove_emoji.py > emoji_removed.txt
-python remove_digits.py > digits_removed.txt
-python remove_punctuation.py > punctuation_removed.txt
